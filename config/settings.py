32d44bcdedd01ae26b0e79840943512b96cc3e28
Igor-prog41 <61401897+Igor-prog41@users.noreply.github.com>
2026-01-11 09:43:33 -0700

    Update settings.py
    Temporarily enable debugging True

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e6&k47bdgo85&&hg!4i7+269fvgyj-j@sx*c@m0w^nolehk!#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
