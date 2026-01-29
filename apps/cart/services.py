5083326f1a1fb561514c454971eb3ae0e518e8bc
Igor Iaroshevych <iaroshevych.i@gmail.com>
2026-01-28 21:51:21 -0700

    feat(cart): add cart actions, service layer logic, and improve add-to-cart UI

--- apps/cart/services.py
+++ apps/cart/services.py
@@ -2,6 +2,19 @@
 from .models import Cart, CartItem
 
 
+#user cart contents or session cart contents
+def get_cart(request):
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user=request.user).first()
+    else:
+        session_id = request.session.session_key
+        if not session_id:
+            request.session.create()
+            session_id = request.session.session_key
+        return Cart.objects.filter(session_key=session_id).first()
+
+
+# creates a user's shopping cart if he is not identified, a session cart
 def get_or_create_cart(request):
     if request.user.is_authenticated:
         cart, _ = Cart.objects.get_or_create(user=request.user)
@@ -12,3 +25,16 @@ def get_or_create_cart(request):
 
     cart, _ = Cart.objects.get_or_create(session_key=request.session.session_key)
     return cart
+
+
+# adds a product to the cart or increases by one
+def add_product_to_cart(cart, product):
+    item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product,
+        defaults={"quantity": 1}
+    )
+
+    if not created:
+        item.quantity += 1
+        item.save()
--- apps/cart/views_http.py
+++ apps/cart/views_http.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from .services import get_or_create_cart
+from django.shortcuts import render,redirect, get_object_or_404
+from .services import get_cart, get_or_create_cart, add_product_to_cart
+
+from apps.catalog.models import Product
+
 
 
 
 def cart_view_http(request):
-    cart = get_or_create_cart(request)
-    items = cart.items.select_related("product")
+    cart = get_cart(request)
+    items = cart.items.select_related("product") if cart else []
 
-    total_price = sum(item.quantity * item.price_snapshot for item in items)
+    total_price = sum(item.quantity * item.product.price for item in items)
 
     return render(request, "cart/cart.html", {
         "cart": cart,
@@ -15,3 +18,26 @@ def cart_view_http(request):
         "total_price": total_price
     })
 
+
+def add_to_cart_view_http(request):
+    if request.method != "POST":
+        return redirect("catalog")
+
+    product_id = request.POST.get("product_id")
+    if not product_id:
+        return redirect(request.META.get("HTTP_REFERER", "catalog"))
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart = get_or_create_cart(request)
+    add_product_to_cart(cart, product)
+
+    return redirect(request.META.get("HTTP_REFERER", "catalog"))
+
+
+def decrease_cart_view_http():
+    return None
+
+
+def remove_from_cart_view_http():
+    return None
